3eb70e2142dcd096e1a0f4f5f0443ec1769ca546
zhouyonghui <zyh@treeholeapp.cn>
2017-04-14 12:56:35 +0800

    header-bulletin

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -2,6 +2,7 @@
 	<div class="header">
 		<div class="content-wrapper">
 			<div class="avatar">
+				<!-- 不能直接用src -->
 				<img width="64" height="64" :src="seller.avatar" alt="">
 			</div>
 			<div class="content">
@@ -13,13 +14,21 @@
 					{{seller.description}}/{{seller.deliveryTime}}
 				</div>
 				<div v-if="seller.supports" class="support">
-					<span class="icon :class="classMap[seller.supports[0].type]></span>
-					<span class="text">{{seller.suppports[0].description</span>
+					<span class="icon" :class="classMap[seller.supports[0].type]"></span>
+					<span class="text">{{seller.supports[0].description}}</span>
 				</div>
+			</div>
+			<div v-if="seller.supports" class="support-count">
+				<span class="count">{{seller.supports.length}}个</span>
+				<i class="icon-keyboard_arrow_right"></i>
 			</div>
 		</div>
 
-		<div class="bulletin-wrapper"></div>
+		<div class="bulletin-wrapper">
+			<!-- 同行消除inline-block带来的间隙 -->
+			<span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+			<i class="icon-keyboard_arrow_right"></i>
+		</div>
 	</div>
 </template>
 
@@ -39,9 +48,11 @@
 @import "../../common/stylus/mixin"
 	.header
 		color: #fff
-		background: #000
+		background: #999
 		.content-wrapper
+			position: relative
 			padding: 24px 12px 18px 24px
+			// 消除inline-block时产生的间隙
 			font-size: 0
 			.avatar
 				display: inline-block
@@ -52,53 +63,99 @@
 				display: inline-block
 				margin-left: 16px
 				font-size: 14px
-			.title
-				margin: 2px 0 8px 0
-				.brand
-					// 内联元素无法设置宽高
-					display: inline-block
-					// 把元素的顶端与行中(行高18)最高元素的顶端对齐
-					vertical-align: top
-					width: 30px 
-					height: 18px
-					bg-image('brand')
-					background-size: 30px 18px
-					background-repaet: no-repeat
-				.name
-					margin-left: 6px
-					font-size: 16px
-					line-height: 18px
-					font-weight: bold
-			.description
-				margin-bottom: 10px
-				line-height: 12px
-				font-size: 12px
-			.support
-				.icon
-					display: inline-block
-					vertical-align: top
-					width: 12px
-					height: 12px
-					margin-right: 4px
-					background-size: 12px
-					backgroung-repeat: no-repeat
-					// 父样式的子样式
-					&.decrease
-						bg-image('decrease_1')
-					&.discount
-						bg-image('discount_1')
-					&.guarantee
-						bg-image('guarantee_1')
-					&.special
-						bg-image('special_1')
-					&.invoice
-						bg-image('invoice_1')
-				.text
+				.title
+					margin: 2px 0 8px 0
+					.brand
+						// 内联元素无法设置宽高
+						display: inline-block
+						// 把元素的顶端与行中(行高18)最高元素的顶端对齐
+						vertical-align: top
+						width: 30px 
+						height: 18px
+						bg-image('brand')
+						background-size: 30px 18px
+						background-repaet: no-repeat
+					.name
+						margin-left: 6px
+						line-height: 18px
+						font-size: 16px				
+						font-weight: bold
+				.description
+					margin-bottom: 10px
 					line-height: 12px
 					font-size: 12px
-					
-					
-						
+				.support
+					.icon
+						display: inline-block
+						vertical-align: middle
+						width: 12px
+						height: 12px
+						margin-right: 4px
+						background-size: 12px
+						backgroung-repeat: no-repeat
+						// 父样式的子样式
+						&.decrease
+							bg-image('decrease_1')
+						&.discount
+							bg-image('discount_1')
+						&.guarantee
+							bg-image('guarantee_1')
+						&.special
+							bg-image('special_1')
+						&.invoice
+							bg-image('invoice_1')
+					.text
+						line-height: 12px
+						font-size: 10px
+			.support-count
+				position: absolute
+				right: 12px
+				bottom: 16px
+				padding: 0 8px
+				height: 24px
+				line-height: 24px
+				border-radius: 14px
+				background: rgba(0,0,0,0.2)
+				text-align: center
+				.count
+					vertical-align: top
+					font-size: 10px
+				.icon-keyboard_arrow_right
+					margin-left: 2px
+					line-height: 24px
+					font-size: 10px
+		.bulletin-wrapper
+			position: relative
+			height: 28px
+			line-height: 28px
+			padding: 0 22px 0 12px
+			// 折叠行 空白被合并
+			white-space: nowrap
+			// 超出父元素隐藏
+			overflow: hidden
+			// 。。。。效果，需要前面两个组合设置
+			text-overflow: ellipsis
+			// 设置为0时。文本中省略号会消失。可在html中让两个span同行，来消除inlin-block带来的空白
+			// font-size: 0	
+			background: rgba(7,17,27,0.2)
+			.bulletin-title
+				display: inline-block
+				margin-top: 7px
+				vertical-align: top
+				width: 	22px
+				height: 12px
+				bg-image('bulletin')
+				background-size: 22px 12px
+				background-repeat: no-repeat			
+			.bulletin-text
+				vertical-align: top
+				margin: 0 4px
+				font-size: 10px	
+			.icon-keyboard_arrow_right
+				position: absolute
+				font-size: 10px
+				right: 12px
+				top: 8px
 				
 		
 </style>
