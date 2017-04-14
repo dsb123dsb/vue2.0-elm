91f9b4094abdca90e7beb67ab042acf06287bd16
zhouyonghui <zyh@treeholeapp.cn>
2017-04-14 13:18:04 +0800

    header-bg

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -29,6 +29,9 @@
 			<span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
 			<i class="icon-keyboard_arrow_right"></i>
 		</div>
+		<div class="background">
+			<img :src="seller.avatar" width="100%" height="100%">
+		</div>
 	</div>
 </template>
 
@@ -47,8 +50,9 @@
 <style lang="stylus" rel="stylesheet/stylus">
 @import "../../common/stylus/mixin"
 	.header
+		position: relative
 		color: #fff
-		background: #999
+		background: rgba(7,17,27,0.5)
 		.content-wrapper
 			position: relative
 			padding: 24px 12px 18px 24px
@@ -156,6 +160,13 @@
 				font-size: 10px
 				right: 12px
 				top: 8px
-				
-		
+		.background
+			position: absolute		
+			left: 0
+			top: 0
+			width: 100%
+			height: 100%
+			z-index: -1
+			// 背景图模糊
+			filter: blur(10px)
 </style>
